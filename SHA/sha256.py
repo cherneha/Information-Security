028ad401b5ed5853286ede5c23f5da259571efaf
cherneha <staceychernega@gmail.com>
2019-10-08 23:18:13 +0300

    Implement Sha256

--- SHA/sha256.py
+++ SHA/sha256.py
@@ -12,18 +12,31 @@ constants = np.array([ 0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25
                        0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2],
                        dtype=np.uint32)
 
-
-
-def leftshift(ba, count):
-    return ba[count:] + (bitarray('0') * count)
-
 def rightshift(ba, count):
     return (bitarray('0') * count) + ba[:-count]
 
-def moduloAddition(self, *args):
-    
-    for
+def rotr(bin, n):
+    return bin[n:] + bin[:n]
 
+def moduloAddition(*args):
+    sum = bitarray(32 * '0')
+    for arg in args:
+        shift = 0
+        for i in range(31, -1, -1):
+            # print(int(arg[i]))
+            # print(int(sum[i]))
+            bit_sum = int(arg[i]) + int(sum[i]) + shift
+            # print("bit sum = ", bit_sum)
+            # print("bit_sum % 2 = ", bit_sum % 2, " ", str(bit_sum % 2))
+            sum[i] = bool(bit_sum % 2)
+            # print("new sum[i] = ", sum[i])
+            # print("shift = ", shift)
+            if bit_sum > 1:
+                shift = 1
+            else:
+                shift = 0
+        # print(sum)
+    return sum
 
 def get_K(L):
     rest = (L + 1 + 64) % 512
@@ -38,38 +51,38 @@ def prepare(message):
     m.append(True)
     m.extend([False] * get_K(L))
     end = bitarray(format(L, 'b'), endian='big')
-    print(end)
+    # print(end)
     p = bitarray(64 - len(end))
     end = p + end
-    print(p)
-    print(end)
+    # print(p)
+    # print(end)
     m = m + end
     return m
 
 def process_in_chunks(m):
-    h0 = np.uint32(0x6A09E667)
-    h1 = np.uint32(0xBB67AE85)
-    h2 = np.uint32(0x3C6EF372)
-    h3 = np.uint32(0xA54FF53A)
-    h4 = np.uint32(0x510E527F)
-    h5 = np.uint32(0x9B05688C)
-    h6 = np.uint32(0x1F83D9AB)
-    h7 = np.uint32(0x5BE0CD19)
+    h0 = bitarray(format(0x6A09E667, '0>32b'))
+    h1 = bitarray(format(0xBB67AE85, '0>32b'))
+    h2 = bitarray(format(0x3C6EF372, '0>32b'))
+    h3 = bitarray(format(0xA54FF53A, '0>32b'))
+    h4 = bitarray(format(0x510E527F, '0>32b'))
+    h5 = bitarray(format(0x9B05688C, '0>32b'))
+    h6 = bitarray(format(0x1F83D9AB, '0>32b'))
+    h7 = bitarray(format(0x5BE0CD19, '0>32b'))
 
     n = math.floor(len(m) / 512)
-    w = [bytearray(False * 32)] * 64
+    w = [bitarray(False * 32)] * 64
     for i in range (0, n):
         chunk = m[i * 512 : (i + 1) * 512]
         for j in range(0, 16):
             slice = chunk[j * 32 : (j + 1) * 32]
             w[j] = slice
         for j in range (16, 64):
-            r1 = rightshift(w[j - 15], 7)
-            r2 = rightshift(w[j - 15], 18)
+            r1 = rotr(w[j - 15], 7)
+            r2 = rotr(w[j - 15], 18)
             r3 = rightshift(w[j - 15], 3)
             s0 = r1 ^ r2 ^ r3
-            s1 = rightshift(w[j - 2], 17) ^ rightshift(w[j - 2], 19) ^ rightshift(w[j - 2], 10)
-            w[j] = moduloAddition(w[j - 16],  s0, w[j - 7] + s1)
+            s1 = rotr(w[j - 2], 17) ^ rotr(w[j - 2], 19) ^ rightshift(w[j - 2], 10)
+            w[j] = moduloAddition(w[j - 16],  s0, w[j - 7], s1)
 
         a = h0
         b = h1
@@ -81,10 +94,12 @@ def process_in_chunks(m):
         h = h7
 
         for i in range(0, 64):
-            S1 = rightshift(e, 6) ^ rightshift(e, 11) ^ rightshift(e, 25)
+            S1 = rotr(e, 6) ^ \
+                 rotr(e, 11) ^ \
+                 rotr(e, 25)
             ch = (e & f) ^ (~e & g)
-            temp1 = moduloAddition(h, S1, ch, constants[i], w[i])
-            S0 = rightshift(a, 2) ^ rightshift(a, 13) ^ rightshift(a, 22)
+            temp1 = moduloAddition(h, S1, ch, bitarray(format(constants[i], '0>32b')), w[i])
+            S0 = rotr(a, 2) ^ rotr(a, 13) ^ rotr(a, 22)
             maj = (a & b) ^ (a & c) ^ (b & c)
             temp2 = moduloAddition(S0, maj)
 
@@ -112,4 +127,10 @@ def process_in_chunks(m):
 if __name__ == '__main__':
     m = prepare("")
     hash = process_in_chunks(m)
-    print(hash)
+
+    hex = ""
+    for i in range(0, 64):
+        block = hash[i * 4: (i + 1) * 4]
+        num = block[0] * 8 + block[1] * 4 + block[2] * 2 + block[3] * 1
+        hex += str(format(num, 'x'))
+    print(hex)
--- SHA/test_sha_256.py
+++ SHA/test_sha_256.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 import sha256
+from bitarray import bitarray
+
 
 class TestPrep(TestCase):
     def test_get_K(self):
         self.assertEqual(sha256.get_K(28), 419)
         self.assertEqual(sha256.get_K(1128), 343)
         self.assertEqual(sha256.get_K(447), 0)
+
+
+class TestModuloAddition(TestCase):
+    def test_moduloAddition(self):
+        self.assertEqual(sha256.moduloAddition(bitarray('1' * 32), bitarray('11111111')), bitarray('01010100'))
