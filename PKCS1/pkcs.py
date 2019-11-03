0104d1f361f8c3e55ac0f4fb854501b800728519
cherneha <staceychernega@gmail.com>
2019-11-04 00:21:27 +0200

    Added RSA key generation. Implemented fast exponentiation.

--- PKCS1/pkcs.py
+++ PKCS1/pkcs.py
@@ -1,5 +1,7 @@
 import SHA.sha256 as sha256
 import math
+import time
+import random as rn
 from bitarray import bitarray
 from Crypto.PublicKey import RSA
 
@@ -55,15 +57,85 @@ def I2OSP(x, xLen):
         X = bitarray('0' * (xLen * 8 - len(X))) + X
     return X
 
+
+def power(x, y, p):
+    res = 1
+    x = x % p
+    while y > 0:
+        if (y & 1) == 1:
+            res = (res * x) % p
+        y = y >> 1
+        x = (x * x) % p
+
+    return res
+
+
 def RSASP1(K, m):
     n, d = K
-    s = (m ** d) % n
+    s = power(m, d, n)
     return s
+
 
 def RSAVP1(public_key, s):
     n, e = public_key
-    m = (s ** e) % n
+    m = power(s, e, n)
     return m
+
+
+def extended_euclid(a, b):
+    if (b == 0):
+        return a, 1, 0
+    d, x, y = extended_euclid(b, a % b)
+    return d, y, x - (a // b) * y
+    # if b == 0:
+    #     return (a, 1, 0)
+    # else:
+    #     d, x, y = extended_euclid(b, a % b)
+    #     return (d, y, x - math.floor(a / b) * y)
+
+def is_prime(n):
+    if n % 2 == 0:
+        return False
+    for i in range(3, math.floor(math.sqrt(n)), 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def miller_rabin(n):
+    if n == 2 or n == 3:
+        return True
+    if n <= 1 or n % 2 == 0:
+        return False
+    s = 0
+    r = n - 1
+    while r & 1 == 0:
+        s += 1
+        r //= 2
+
+    for i in range(0, 1500):
+        a = rn.randrange(2, n - 1)
+        x = power(a, r, n)
+        if x != 1 and x != n - 1:
+            for j in range(0, s):
+                x = power(x, 2, n)
+                if x != n - 1:
+                    return False
+    return True
+
+def generate_prime_candidate(length):
+    x = rn.getrandbits(length)
+    x |= (1 << length - 1) | 1
+    return x
+
+def generate_prime(length=512):
+    i = 0
+    while True:
+        p = generate_prime_candidate(length)
+        i += 1
+        if miller_rabin(p):
+            # if is_prime(p):
+            return p
 
 def RSASSA_PKCS1_V1_5_VERIFY(public_key, M, S):
     n, e, modBits = public_key
@@ -77,14 +149,33 @@ def RSASSA_PKCS1_V1_5_VERIFY(public_key, M, S):
     else:
         print("Invalid signature")
 
+def gen_keys():
+    n = 0
+    while len(bitarray(format(n, 'b'))) != 1024:
+        p = generate_prime(510)
+        q = generate_prime(514)
+        n = p * q
+    print("p = ", p)
+    print("q = ", q)
+    fi_n = (p - 1) * (q - 1)
+    print("fi(n) = ", fi_n)
+    print("n = ", n)
+    e = 65537
+    res = extended_euclid(e, fi_n)
+    print(res)
+    d = res[1]
+    if d < 0:
+        d = d + n
+    print("d = ", d)
+    print("n len = ", len(bitarray(format(n, 'b'))))
+    return (e, d, n)
+
+
 if __name__ == '__main__':
-    key = RSA.generate(1024)
-    privateKey = key.exportKey('DER')
-    publicKey = key.publickey().exportKey('DER')
-    n = key.n
-    d = key.d
-    e = key.e
+    start = time.time()
+    e, d, n = gen_keys()
     message = "happy kitty, sleepy kitty"
     signature = RSASSA_PKCS1_V1_5_SIGN((n, d), message)
     RSASSA_PKCS1_V1_5_VERIFY((n, e, 1024), message, signature)
-
+    end = time.time()
+    print("time elapsed = ", end - start)
